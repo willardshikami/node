3512dab2fd4a08ed980f2e3a7d9cfd8cb338f3b6
willardshikami <willardshikami@gmail.com>
2017-04-05 01:25:42 +0300

    created a server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-require('./app/index'); 
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World, This is dope');
+});
+
+server.listen(3000, '127.0.0.1');
+console.log("Just created a server")
