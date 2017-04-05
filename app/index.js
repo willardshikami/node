9bfaa617aae26d6cd3088b9a7cf8d7dfd70b8fa8
willardshikami <willardshikami@gmail.com>
2017-04-05 03:06:26 +0300

    returned a simple javascript function

---
+++ app/index.js
@@ -0,0 +1,10 @@
+
+const profile = {
+    name : "willard",
+    occupation : "software engineer",
+    age : 25
+}
+
+for(key in profile){
+    console.log(key + ":" + profile[key]);
+}
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+require('./app/index');
+//calling a http module
 var http = require('http');
 
+//method to create a server
 var server = http.createServer(function(req, res){
+    //a response header (status, object containing content type)
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    //method to end response and send it to the browser.
     res.end('Hello World, This is dope');
 });
 
+//listening to a call so as to fire the createServer function
 server.listen(3000, '127.0.0.1');
-console.log("Just created a server")
+console.log("Just created a server");
